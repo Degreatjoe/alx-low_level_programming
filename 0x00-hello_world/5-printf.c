f9f55a88b61640010dcddab761bdc02a08d126d6
Degreatjoe <jgreat770@gmail.com>
2023-07-13 20:43:07 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
-/*
- * description
- * function main - Entry_point
- * Return: Always 0
- *
+/**
+ * main - the entry point to the programme that prints out using
+ * printf function
+ **
+ * Return: 0 (Success) -end point
  */
 int main(void)
-/*
- * description
- * main - entry_point
- */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-/*
- * main - entry_point
- * Return: Always 0
- *
- */
