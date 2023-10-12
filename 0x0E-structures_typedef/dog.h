740e95b7d407cd63058ce848fccb767c3e7fb992
Degreatjoe <jgreat770@gmail.com>
2023-10-12 13:57:48 +0100

    task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ struct dog
 /**
  * dog_i - typedef for struct dog
  */
-typedef struct dog dog_i;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
