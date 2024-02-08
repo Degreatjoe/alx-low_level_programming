3ffa90ea98d5c5653afdb708bfee18ebf4a19c07
Degreatjoe <jgreat770@gmail.com>
2024-02-08 11:54:37 +0100

    task #001

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int number;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int d, ch;
+	char flag;
+
+	flag = 0;
+	d = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (d != 0)
+	{
+		ch = n & d;
+		if (ch == d)
+		{
+		flag = 1;
+		_putchar('1');
+		}
+		else if (flag == 1 || d == 1)
+		{
+			_putchar('0');
+		}
+		d >>= 1;
+	}
+}
