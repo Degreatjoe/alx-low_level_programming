354559cb58577015d0fd74be2f18e5b5f7d1e8d1
Degreatjoe <jgreat770@gmail.com>
2024-02-13 13:23:26 +0100

    task #003

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,47 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
+#define BUFFER_SIZE 1024
+
+char *create_buffer(void);
 void close_file(int fd);
-
-/**
- * creates_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
- */
-char *creates_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
-
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
 
 /**
  * main - Copies the contents of a file to another file.
@@ -58,47 +21,92 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
+	int from, to;
+	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
+    /* Check if the correct number of arguments is provided */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[2]);
+    /* Allocate buffer */
+	buffer = create_buffer();
+
+    /* Open source file for reading */
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+    /* Open destination file for writing, create if not exist */
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_file(from);
+		exit(99);
+	}
 
-	do {
-		if (from == -1 || r == -1)
+    /* Copy content from source file to destination file */
+	while ((bytes_read = read(from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close_file(from);
+			close_file(to);
 			exit(99);
 		}
+	}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_file(from);
+		close_file(to);
+		exit(98);
+	}
 
-	} while (r > 0);
-
-	free(buffer);
+    /* Close file descriptors */
 	close_file(from);
 	close_file(to);
 
+	free(buffer);
+
 	return (0);
 }
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(void)
+{
+	char *buffer = malloc(BUFFER_SIZE);
 
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't allocate buffer\n");
+		exit(99);
+	}
+	return (buffer);
+}
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
