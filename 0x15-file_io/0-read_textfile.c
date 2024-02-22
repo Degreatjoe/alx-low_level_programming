838f18ec9abf618a47cd8a804d2d9f0a82791229
Degreatjoe <jgreat770@gmail.com>
2024-02-22 06:13:18 +0100

    from atom

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,7 @@
 
 /**
  * read_textfile- Read text file print to STDOUT.
+
  * @filename: text file being read
  * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
@@ -26,4 +27,3 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 	return (w);
 }
-
